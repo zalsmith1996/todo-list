94d60db104e1cd84867d3b06cb04068c9282c2ce
Zack <107967808+zalsmith1996@users.noreply.github.com>
2024-06-07 07:31:23 -0400

    initial setup and configuration

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/modules/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+        ],
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        client: {
+            overlay: {
+                warnings: true,
+                errors: true,
+            },
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Todo App',
+            template: './src/index.html',
+            inject: 'head',
+            scriptLoading: 'defer',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: './src/assets', to: './assets', force: true }],
+        }),
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
