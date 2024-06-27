915e4676ac3f66984d7c25ff175e525e4966bf2c
Zack <107967808+zalsmith1996@users.noreply.github.com>
2024-06-27 17:11:29 -0400

    update styling, remove unecessary svg, update storage file

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,2 +1,7 @@
- // If project list object is not in local storage, create one
- // Initialize it to have a default project with example tasks
+import { Project } from './project.js';
+import { Task } from './task.js';
+
+
+export default class Storage {
+    
+};
