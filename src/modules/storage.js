215cbb29d82064944e1a92c85968dc7ee0b2d2fa
Zack <107967808+zalsmith1996@users.noreply.github.com>
2024-07-03 17:22:43 -0400

    add ability to use local storage, update styling

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,16 +1,45 @@
 import Project from './project.js';
 import Task from './task.js';
 
-// Create default project list and store in localStorage
-const projects = [new Project('Default', {'Example Task': new Task('Example Task', '2024/09/17', 'Example description')})];
-updateProjects(projects);
+const d = document;
+const projectListing = d.querySelector('.projects');
+let projects = [];
 
-    // Check to see if localStorage.projects exists, 
-    // If yes, use what is store to populate UI,
-    // If no, generate default project list and store in localStorage as a key/value "projects"/{} 
-    // {} value will be used to store projects as key/value with projectName/{tasks}
+// Create default project list and store in localStorage
+
+if (localStorage.getItem('projects') === null) {
+    projects = [new Project('Default', {'Example Task': new Task('Example Task', '2024/09/17', 'Example description')})];
+    updateProjects(projects);
+} else {
+    projects = JSON.parse(localStorage.getItem('projects'));
+    updateProjects(projects);
+}
+
+
 
 export function updateProjects(projectArray) {
     localStorage.setItem('projects', JSON.stringify(projectArray));
-    console.log(JSON.parse(localStorage.getItem('projects')));
-}
+
+    projectListing.innerHTML = '';
+
+    for (const project of projects) {
+        const projectDiv = d.createElement('div');
+        const listing = d.createElement('p');
+        projectDiv.classList.add('project-listing');
+        listing.classList.add('project-listing-title');
+        listing.textContent = project.title;
+
+        projectDiv.appendChild(listing);
+        projectListing.append(projectDiv);
+    }
+};
+
+export function addProject() {
+    let projectName = d.querySelector('.project-title');
+    projects.push(new Project(projectName.value, {}));
+    updateProjects(projects);
+    projectName.value = '';
+};
+
+
+// When a project is active (being displayed) add a delete button to the bottom of the task display window
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,9 +1,23 @@
-import { updateProjects } from './storage.js';
+import { addProject } from './storage.js';
 
 // DOM elements included in case HTML file
 const d = document;
-
+const newProjectPopup = d.querySelector('.new-project-popup');
 
 export function render() {
     //place event listeners here, call in index.js
+    d.body.addEventListener('click', (e) => {
+        if (e.target.matches('.new-project-btn')) {
+            newProjectPopup.style.display = 'block';
+        }
+        if (e.target.matches('.project-add-yes')) {
+            e.preventDefault();
+            addProject();
+            newProjectPopup.style.display = 'none';
+        }
+        if (e.target.matches('.project-add-no')) {
+            e.preventDefault();
+            newProjectPopup.style.display = 'none';
+        }
+    });
 };
